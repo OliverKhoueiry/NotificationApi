be41d3fefefa36d460361dc1cef54e706f611030
oliver khoueiry <oliver.khoueiry13@gmail.com>
2025-07-10 14:43:03 +0300

    register

--- BusinessLayer/IBusinessHandler.cs
+++ BusinessLayer/IBusinessHandler.cs
@@ -9,5 +9,14 @@ namespace BusinessLayer
         Task<(ApiResponse, string?, string?)> LoginAsync(LoginRequest request);
         Task<ApiResponse> ForgetPasswordAsync(string email);
         Task<(ApiResponse, string?, string?)> RefreshTokenAsync(string refreshToken);
+
+
+
+
+
+
+
+
+
     }
 }
